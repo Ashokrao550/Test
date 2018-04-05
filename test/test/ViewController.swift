08eafb864dea1c7e7b4ea30a194f96764105bf20
Pavan Kumar <pavankumarthota1994@achiever-75.local>
2018-04-05 16:55:25 +0530

    hahahaaa

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Multi changes")
-        print("not getting")
+        print("Moosuko")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
