8b549cda6802fa2e93a8760d39521fa9cc69a463
Pavan Kumar <pavankumarthota1994@achiever-75.local>
2018-04-05 16:52:03 +0530

    change

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ramu")
+        print("Multi changes")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
